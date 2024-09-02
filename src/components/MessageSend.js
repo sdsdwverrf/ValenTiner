ec4fe741d3195f51555a5ba1c07081182b339b75
bright <hacoderhu@gmail.com>
2024-09-02 22:21:17 +0530

    Update MessageSend.js

--- src/components/MessageSend.js
+++ src/components/MessageSend.js
@@ -6,7 +6,7 @@ export default function MessageSend() {
             <div className="button-send">
                 <div
                     className="button-text px-7 py-3 text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-tr from-[#3a6186] to-[#89253e] text-center">
-                    Thank you Buddhu ek din jaroor sath me khelnge badminton T_T ache se sekhlena mereko harana easy nahi gurrrrrrrrrrrrr
+                    Thank you Buddhu ek din jaroor sath me khelnge badminton T_T ache se sekhlena mereko harana easy nahi gurrrrrrrrrrrrr aur ha mujhe moon-asa bolna jada pasand hai lol
                 </div>
             </div>
         </div>
