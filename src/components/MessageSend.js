2c510872dc14386b371c2b30096e1d403dd6a8b7
bright <hacoderhu@gmail.com>
2024-09-02 17:29:58 +0530

    Update MessageSend.js

--- src/components/MessageSend.js
+++ src/components/MessageSend.js
@@ -4,10 +4,5 @@ var messageElement = document.createElement("div");
 // Set the text content of the element
 messageElement.textContent = "Thank you Buddhu";
 
-// Style the text to make it bigger and blue
-messageElement.style.fontSize = "48px"; // Set the font size to 48 pixels
-messageElement.style.color = "blue"; // Set the font color to blue
-
 // Add the element to the body of the document
 document.body.appendChild(messageElement);
-//de
