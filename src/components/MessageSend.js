9c46be183529c643b2089929bb0c8f82749a0b4a
bright <hacoderhu@gmail.com>
2024-09-02 17:40:22 +0530

    Update MessageSend.js

--- src/components/MessageSend.js
+++ src/components/MessageSend.js
@@ -1,8 +1,14 @@
-// Create a new element
-var messageElement = document.createElement("div");
+import React from 'react';
 
-// Set the text content of the element
-messageElement.textContent = "Thank you Buddhu";
-
-// Add the element to the body of the document
-document.body.appendChild(messageElement);
+export default function MessageSend() {
+    return (
+        <div className="message-form flex items-center justify-center h-screen">
+            <div className="button-send">
+                <div
+                    className="button-text px-7 py-3 text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-tr from-[#3a6186] to-[#89253e] text-center">
+                    Thank you Buddhu
+                </div>
+            </div>
+        </div>
+    );
+}
